d00cd0229e565efaf4c5ed6ab19c01d17781e23d
Licinio Sousa <liciniosousa14@gmail.com>
2025-01-28 16:44:05 +0000

    pequena alteracao no laravel

--- php/laravel/projeto1/ServerSideSD/resources/views/fallback.blade.php
+++ php/laravel/projeto1/ServerSideSD/resources/views/fallback.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Estar perdido?</h1>
+@extends('layouts.fo_layout')
+
+@section('content')
+    <h1>Estas perdido?</h1>
     <a href="{{ route('home')}}"><h3>Esperimenta clicar aqui.</h3></a>
-</body>
-</html>
+@endsection
