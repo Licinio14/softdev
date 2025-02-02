c4f61ebcdabab85312f32db0b76da59069fda44b
Licinio Sousa <liciniosousa14@gmail.com>
2025-02-02 22:04:31 +0000

    app android

--- android/primeiraAPP/app/src/main/java/com/example/a01_primeiraapp/MainActivity.kt
+++ android/primeiraAPP/app/src/main/java/com/example/a01_primeiraapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a01_primeiraapp
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +19,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.buttonConvert.setOnClickListener {
-            var euro: Double = binding.inputNumber.text.toString().toDouble()
-            var dollar = euro * 1.2
-            binding.textResult.text = "$dollar"
+        binding.btt1.setOnClickListener {
+            if (binding.nameText.text.toString().isEmpty() || binding.surnameText.text.toString().isEmpty()){
+                Toast.makeText(this, "Tem de preencher todos os campos!", Toast.LENGTH_SHORT).show()
+            }else {
+                var name: String = binding.nameText.text.toString()
+                var surname: String = binding.surnameText.text.toString()
+
+                var completeName = "Olá $name $surname"
+                // binding.resultado.text = completeName
+
+                Toast.makeText(this, completeName, Toast.LENGTH_SHORT).show()
+            }
+
+
         }
     }
 }
